af50f08dae4d5e37c4bbfa66fe44388ddf38e37b
muji <noop@xpm.io>
2015-07-13 16:35:01 +0800

    Add alias test spec.

---
+++ test/spec/alias.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect
+  , config = {name: 'mxl'}
+  , pkg = '../../package.json'
+  , program = require('../../lib/mxl');
+
+describe('mxl:', function() {
+
+  it('should list aliases', function(done) {
+    var args = ['alias'];
+    var def = program(require(pkg), config.name)
+    def.program.on('complete', function(req) {
+      done();
+    })
+    def.parse(args);
+  });
+
+});
