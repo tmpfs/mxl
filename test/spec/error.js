ae9c236ab6d4ba5ccd68e49fbf05a986e75bcc6b
muji <noop@xpm.io>
2015-07-14 12:32:25 +0800

    Assert on no files found error.

--- test/spec/error.js
+++ test/spec/error.js
@@ -6,6 +6,21 @@ var expect = require('chai').expect
 describe('mxl:', function() {
 
   it('should error on missing source file', function(done) {
+    var args = ['--no-color', '../missing.tmux.conf'];
+    var def = program(require(pkg), config.name)
+    def.program.on('error', function(err) {
+      expect(err.code).to.eql(1);
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(Error);
+      expect(fn).throws(/no such file or directory/i);
+      done();
+    })
+    def.parse(args);
+  });
+
+  it('should error on missing source file w/ run command', function(done) {
     var args = ['run', '--no-color', '../missing.tmux.conf'];
     var def = program(require(pkg), config.name)
     def.program.on('error', function(err) {
@@ -15,6 +30,20 @@ describe('mxl:', function() {
       }
       expect(fn).throws(Error);
       expect(fn).throws(/no such file or directory/i);
+      done();
+    })
+    def.parse(args);
+  });
+
+  it('should error on no files found', function(done) {
+    var args = ['run', '--no-color', '../empty'];
+    var def = program(require(pkg), config.name)
+    def.program.on('error', function(err) {
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(Error);
+      expect(fn).throws(/no files found/i);
       done();
     })
     def.parse(args);
