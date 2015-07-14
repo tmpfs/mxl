21aea5c2be50341ea5ebbd2dd5f306af40b83f2a
muji <noop@xpm.io>
2015-07-14 16:13:19 +0800

    Simplify error handling.

--- test/global.js
+++ test/global.js
@@ -1,12 +1,40 @@
-var info = console.info;
+var path = require('path')
+  , fs = require('fs')
+  , info = console.info;
 process.chdir('test/fixtures/conf');
+
+function clean() {
+  var names = [
+    'fixtures-error',
+    'conf',
+    'conf-empty',
+    'conf-alt',
+    'index',
+    'foo'
+  ];
+  var file = path.join(process.env.HOME, '.mxlrc.json');
+  if(fs.existsSync(file)) {
+    try {
+      rc = require(file);
+    }catch(e) {}
+
+    if(rc && rc.alias) {
+      names.forEach(function(name) {
+        delete rc.alias[name] ;
+      }) 
+      fs.writeFileSync(file, JSON.stringify(rc, undefined, 2));
+    }
+  }
+}
 
 before(function(done) {
   console.info = function(){};
+  clean();
   done();
 });
 
 after(function(done) {
   console.info = info;
+  clean();
   done();
 });
