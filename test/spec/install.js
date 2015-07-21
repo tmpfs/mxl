804a4078e3721cdfe6879afc30d987ae132cbbdc
muji <noop@xpm.io>
2015-07-21 10:07:49 +0800

    Define @git alias.
    Work on install tests.

--- test/spec/install.js
+++ test/spec/install.js
@@ -16,6 +16,16 @@ describe('mxl:', function() {
     done(); 
   })
 
+  afterEach(function(done) {
+    var files = ['tmux.conf', 'home.tmux.conf'];
+    files.forEach(function(file) {
+      if(fs.existsSync(file)) {
+        fs.unlinkSync(file);
+      }
+    })
+    done();
+  })
+
   it('should run install with system alias reference',
     function(done) {
       var args = ['install', '--no-color', '@home'];
@@ -27,5 +37,59 @@ describe('mxl:', function() {
       def.parse(args);
     }
   );
+
+  it('should run install with system alias reference and default file name',
+    function(done) {
+      var args = ['install', '--no-color', '@home=tmux.conf'];
+      var def = program(require(config.pkg), config.name)
+      def.program.on('complete', function(req) {
+        expect(fs.existsSync('tmux.conf')).to.eql(true);
+        done();
+      })
+      def.parse(args);
+    }
+  );
+
+  it('should run install with system alias reference and custom file name',
+    function(done) {
+      var args = ['install', '--no-color', '@home=home'];
+      var def = program(require(config.pkg), config.name)
+      def.program.on('complete', function(req) {
+        expect(fs.existsSync('home.tmux.conf')).to.eql(true);
+        done();
+      })
+      def.parse(args);
+    }
+  );
+
+  it('should run install with system alias reference and custom name/extension',
+    function(done) {
+      var args = ['install', '--no-color', '@home=home.tmux.conf'];
+      var def = program(require(config.pkg), config.name)
+      def.program.on('complete', function(req) {
+        expect(fs.existsSync('home.tmux.conf')).to.eql(true);
+        done();
+      })
+      def.parse(args);
+    }
+  );
+
+  // errors
+  it('should error on install name conflict', function(done) {
+    var args = ['install', '--no-color', '@vim', '@git'];
+    var def = program(require(config.pkg), config.name)
+    def.program.on('error', function(err) {
+      expect(err.code).to.be.gt(0);
+      console.dir(err)
+      function fn() {
+        throw err;
+      }
+      expect(fn).throws(Error);
+      //expect(fn).throws(/no such file or directory/i);
+      done();
+    })
+    def.parse(args);
+  });
+
 
 });
