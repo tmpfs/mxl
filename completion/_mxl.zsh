a4c8cfccd7ac283a2e5b9f787ad85c7124c6842e
muji <noop@xpm.io>
2015-07-25 21:56:47 +0800

    Update completion.

--- completion/_mxl.zsh
+++ completion/_mxl.zsh
@@ -18,17 +18,18 @@
 
 local -a commands
 commands=(
+  'attach:Attach to a session'
   'ls:List configuration files'
   'source:Source tmux configuration files'
   'alias:Manage file aliases'
   'remove:Remove aliases by pattern match'
   'prune:Remove stale aliases'
   'index:Generate alias index'
-  'install:Install alias files'
   'kill:Kill sessions, windows and panes'
+  'install:Install alias files'
   'reshuffle:Reshuffle window indices to be sequential'
-  'help:Show man pages'
   'quit:Kill the tmux server'
+  'help:Show man pages'
 )
 
 _arguments -C -- \
